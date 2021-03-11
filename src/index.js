7dbfbc132d4c61240314db2317fc119649f9a01a
Iiro Rahkonen <iiro.rahkonen@gmail.com>
2021-03-11 14:03:42 +0200

    Initial commit

--- src/index.js
+++ src/index.js
@@ -1,14 +1,38 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+
+import { Provider, connect } from "react-redux";
+import store from "./store";
+
+import "bootstrap/dist/css/bootstrap.min.css";
+
+const Count = (props) => (
+  <div>
+    The count is {props.count}
+    <button onClick={props.increment}>increment</button>
+    <button onClick={props.incrementAsync}>incrementAsync</button>
+  </div>
+);
+
+const mapState = (state) => ({
+  count: state.count,
+});
+
+const mapDispatch = (dispatch) => ({
+  increment: () => dispatch.count.increment(1),
+  incrementAsync: () => dispatch.count.incrementAsync(1),
+});
+
+const CountContainer = connect(mapState, mapDispatch)(Count);
 
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  </Provider>,
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
